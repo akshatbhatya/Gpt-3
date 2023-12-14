2578b0fbbace9ce74e6821968e086833ef072d75
akshat <akshatbhatya@gmail.com>
2023-12-14 11:53:43 +0530

    add new section and complete cta section

--- project1/src/Components/Container/Possibility/Possibility.jsx
+++ project1/src/Components/Container/Possibility/Possibility.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import "./Possibility.css"
 
+import PossibilitiesPic from "../../../assets/Possibility.png"
+
 function Possibility() {
   return (
-    <div>Possibility</div>
+    <div className='possibility_parent'>
+      <div className="possibility_image">
+        <img src={PossibilitiesPic} alt="PossibilitiesPic" />
+      </div>
+      <div className="possibility_content">
+
+        <div className="para1">
+          <p>Request Early Access to Get Started</p>
+        </div>
+        <br />
+
+        <div className="heading">
+          <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        </div>
+        <br />
+        <div className="para3">
+          <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        </div>
+        <br />
+        <div className="para4">
+          <p>Request Early Access to Get Started</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- project1/src/Components/Cta/Cta.jsx
+++ project1/src/Components/Cta/Cta.jsx
@@ -4,7 +4,16 @@ import "./Cta.css"
 const Cta = () => {
   return (
     <div>
-      Cta
+      <div className="cta-parent">
+        <div className="ctacontent">
+          <p>Request Early Access to Get Started</p>
+          <h1>Register today & start exploring the endless possiblities.</h1>
+        </div>
+        <div className="cta-btn">
+          <button>Get Started</button>
+
+        </div>
+      </div>
     </div>
   )
 }
